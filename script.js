1371db245651cababce7f8dc09157cd6802acd02
FatimaCoulibaly <42788249+FatimaCoulibaly@users.noreply.github.com>
2018-12-11 15:32:57 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+ var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+   document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
